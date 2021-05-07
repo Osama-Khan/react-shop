089f09a2ba77818e92225efed1720685bb9bac6d
Osama Khan <Osama6756@gmail.com>
2021-05-07 17:12:58 +0500

    Added a category-icon map to handle icons of categories

---
+++ src/data/category-icon-map.ts
@@ -0,0 +1,9 @@
+export const catIconMap =
+{
+  "CPU": "whh:cpu",
+  "GPU": "whh:graphicscard",
+  "RAM": "clarity-memory-solid",
+  "Motherboard": "codicon:circuit-board",
+  "PSU": "fa:bolt",
+  "Peripherals": "wpf:mouse",
+}
