85404008a3e220b82b320675174109a2e63f39d3
Osama Khan <Osama6756@gmail.com>
2021-05-08 12:07:56 +0500

    Added ClickableIcon

--- src/app/components/icon/icon.jsx
+++ src/app/components/icon/icon.jsx
@@ -1,3 +1,22 @@
-export default function Icon({dataIcon, classes}) {
-    return (<span className={`iconify ${classes?classes:""}`} data-icon={dataIcon} data-inline="false"></span>);
+export default function Icon({ dataIcon, classes, click }) {
+  return click ? (
+    ClickableIcon({ dataIcon, classes, click })
+  ) : (
+    <span
+      className={`iconify ${classes ? classes : ""}`}
+      data-icon={dataIcon}
+      data-inline="false"
+    ></span>
+  );
+}
+
+function ClickableIcon({ dataIcon, classes, click }) {
+  return (
+    <span
+      className={`iconify clickable ${classes ? classes : ""}`}
+      data-icon={dataIcon}
+      data-inline="false"
+      onClick={click?click:""}
+    ></span>
+  );
 }
