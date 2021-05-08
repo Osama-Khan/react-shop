21d335a003a0289c41b49f8551369643a80d8994
Osama Khan <Osama6756@gmail.com>
2021-05-08 06:47:43 +0500

    Added function to fetch product by ID

--- src/app/services/product-service.ts
+++ src/app/services/product-service.ts
@@ -1,9 +1,18 @@
 import Service from "./service";
 
 export default class ProductService extends Service {
+  url = `https://api.ghostware.xyz/test/products.php?key=${this.accessKey}`;
+  
   async fetchProducts() {
-    const url = `https://api.ghostware.xyz/test/products.php?key=${this.accessKey}`;
-    let p = await fetch(url).then((r) => {
+    let p = await fetch(this.url).then((r) => {
+      return r.json();
+    })
+    return p;
+  }
+
+  async fetchProduct(id: number) {
+    const params = `&id=${id}`;
+    let p = await fetch(this.url + params).then((r) => {
       return r.json();
     })
     return p;
