a477e8094998c2b70b15db5d3235e644e086ff09
Osama Khan <Osama6756@gmail.com>
2021-05-08 13:37:01 +0500

    Added Popup component

---
+++ src/app/components/popup/popup.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import P from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+ 
+export default function Popup({ trigger, position = "bottom center", content}) {
+  return (
+  <P trigger={trigger} position={position}>
+    {content}
+  </P>
+  );
+}
