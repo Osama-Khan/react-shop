c6834ef46c1287345797bad4ce0d60257a4be980
Osama Khan <Osama6756@gmail.com>
2021-05-09 13:19:24 +0500

    Added a model for CartProduct

---
+++ src/app/models/cart-product.ts
@@ -0,0 +1,17 @@
+import Product from "./product";
+
+export default class CartProduct {
+  id: number;
+  title: string;
+  description: string;
+  quantity: number;
+  price: number;
+
+  constructor(p: Product, quantity = 1) {
+    this.id = p.id;
+    this.title = p.title;
+    this.description = p.description;
+    this.quantity = quantity;
+    this.price = p.price;
+  }
+}
