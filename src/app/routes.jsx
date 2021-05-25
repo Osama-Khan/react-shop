b0718921089e0ea91a384a3ce4b4374e2b63dee1
Osama Khan <Osama6756@gmail.com>
2021-05-25 15:48:55 +0500

    Added user and register endpoints

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -1,15 +1,21 @@
 import Categories from "./categories/categories";
 import Products from "./products/products";
 import Home from "./home/Home";
+import User from "./user/user";
+import Register from "./user/register";
 
 export const homeUrl = "/";
 export const productsUrl = "/products";
 export const categoriesUrl = `/categories`;
+export const userUrl = "/user";
+export const registerUrl = "/register";
 
 export const routes = [
-    { path: homeUrl, component: Home },
-    { path: productsUrl, component: Products },
-    { path: productsUrl + "/:id", component: Products },
-    { path: categoriesUrl, component: Categories },
-    { path: categoriesUrl + "/:id", component: Categories },
-]
+  { path: homeUrl, component: Home },
+  { path: productsUrl, component: Products },
+  { path: productsUrl + "/:id", component: Products },
+  { path: categoriesUrl, component: Categories },
+  { path: categoriesUrl + "/:id", component: Categories },
+  { path: userUrl, component: User },
+  { path: registerUrl, component: Register },
+];
