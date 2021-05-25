b515eb621b788c20198ed5f1278926e03d991cad
Osama Khan <Osama6756@gmail.com>
2021-05-25 18:55:04 +0500

    Implemented categories component

--- src/app/categories/categories.jsx
+++ src/app/categories/categories.jsx
@@ -1,9 +1,58 @@
+import { Component } from "react";
+import { catIconMap } from "../../data/category-icon-map";
+import Icon from "../components/icon/icon";
+import Card from "../components/card/card";
+import { AppContext } from "../context/app.provider";
+import { categoriesUrl } from "../routes";
 
+export default class Categories extends Component {
+  static contextType = AppContext;
 
-export default function Categories() {
+  constructor(props) {
+    super(props);
+    this.state = { categories: null };
+  }
+
+  componentDidMount() {
+    this.context.services.categoryService
+      .fetchCategories()
+      .then((categories) => {
+        this.setState({ categories });
+      });
+  }
+
+  render() {
+    const catPage = this.state.categories
+      ? this.renderCategories()
+      : this.renderLoadingIcon();
+    console.log(catPage);
     return (
-        <div className="row mt-5">
-            <div className="col-12 card">Categories page</div>
-        </div>
+      <>
+        <h1 className="mt-5">Categories</h1>
+        <div className="row mt-5">{catPage}</div>
+      </>
     );
+  }
+
+  renderCategories() {
+    const categories = this.state.categories;
+    const mainCats = categories.map((c) => (
+      <div key={c.id} className="col-6 col-md-4 col-lg-2">
+        <Card
+          text={c.name}
+          color="primary"
+          linkTo={`${categoriesUrl}/${c.name.toLowerCase()}`}
+        />
+      </div>
+    ));
+    return mainCats;
+  }
+
+  renderLoadingIcon() {
+    return (
+      <div key="loadingIcon" className="col-12 d-flex my-5 py-5">
+        <Icon classes="mx-auto icon-lg spin" dataIcon="fa:spinner" />
+      </div>
+    );
+  }
 }
