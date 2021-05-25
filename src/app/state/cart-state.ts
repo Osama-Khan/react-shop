a8562fe85e2030326dcc27860c50676310519642
Osama Khan <Osama6756@gmail.com>
2021-05-25 15:43:09 +0500

    Improved CartState

--- src/app/state/cart-state.ts
+++ src/app/state/cart-state.ts
@@ -10,7 +10,7 @@ export default class CartState {
    * @param p The product to add
    * @param q Number of products to add
    */
-  addProduct(p: Product, q = 1): void {
+  addProduct = (p: Product, q = 1): void => {
     let exists = false
     this.products = this.products.map((prd) => {
       if (prd.id === p.id) {
@@ -29,7 +29,7 @@ export default class CartState {
    * Removes a product from the cart
    * @param id ID of the product to remove
    */
-  removeProduct(id: number): void {
+  removeProduct = (id: number): void => {
     const p = this.products.filter((p) => p.id !== id);
     if (p.length !== this.products.length) {
       // Inform the user that product was removed
@@ -44,7 +44,7 @@ export default class CartState {
    * @param id ID of the product to look for
    * @returns True or false, depending on if the product is present or not respectively
    */
-  hasProduct(id: number): boolean {
+  hasProduct = (id: number): boolean => {
     return (this.products.some((p) => p.id === id));
   }
 }
