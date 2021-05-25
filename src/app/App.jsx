197bb6d81eecc1099632a356c8ffa082edad1d6c
Osama Khan <Osama6756@gmail.com>
2021-05-25 15:41:10 +0500

    Added AppProvider for context

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,30 +1,33 @@
-import React from 'react'
-import Navbar from './components/navbar/Navbar';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { AnimatedSwitch } from 'react-router-transition';
-import { routes as r } from './routes';
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { AnimatedSwitch } from "react-router-transition";
+import { routes as r } from "./routes";
+import { Toaster } from "react-hot-toast";
+import { AppProvider } from "./context/app.provider";
 
-function App() {
-  const routes = r.map((r, i) => (
-    <Route key={`route-${i}`} exact path={r.path} component={r.component} />
-  ));
-  return (
-    <div className="app">
-      <Router>
-      <Navbar/>
-      <div className="container">
-          <AnimatedSwitch
-            atEnter={{ opacity: 0 }}
-            atLeave={{ opacity: 0 }}
-            atActive={{ opacity: 1 }}
-            className="switch-wrapper"
-          >
-            {routes}
-          </AnimatedSwitch>
-      </div>
-      </Router>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    const routes = r.map((r, i) => (
+      <Route key={`route-${i}`} exact path={r.path} component={r.component} />
+    ));
+    return (
+      <AppProvider>
+        <div className="app">
+          <Router>
+            <Navbar />
+            <div className="container">
+              <AnimatedSwitch
+                atEnter={{ opacity: 0 }}
+                atLeave={{ opacity: 0 }}
+                atActive={{ opacity: 1 }}
+                className="switch-wrapper">
+                {routes}
+              </AnimatedSwitch>
+            </div>
+          </Router>
+        </div>
+      </AppProvider>
+    );
+  }
 }
-
-export default App;
---
+++ src/app/context/app.provider.jsx
@@ -0,0 +1,25 @@
+import { useState, createContext } from "react";
+import CategoryService from "../services/category.service";
+import ProductService from "../services/product.service";
+import UiService from "../services/ui.service";
+import UserService from "../services/user.service";
+import State from "../state/state";
+
+export const AppContext = createContext();
+
+export const AppProvider = (props) => {
+  const [state, setState] = useState(new State());
+  const context = {
+    services: {
+      categoryService: new CategoryService(),
+      productService: new ProductService(),
+      uiService: new UiService(),
+      userService: new UserService(),
+    },
+    state,
+    setState,
+  };
+  return (
+    <AppContext.Provider value={context}>{props.children}</AppContext.Provider>
+  );
+};
