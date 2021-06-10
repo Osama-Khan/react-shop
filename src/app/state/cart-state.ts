d60587b7ebe8b763a665ce31f145c4d2847b0400
Osama Khan <Osama6756@gmail.com>
2021-06-10 11:45:39 +0500

    Added clearCart method

--- src/app/state/cart-state.ts
+++ src/app/state/cart-state.ts
@@ -11,9 +11,9 @@ export default class CartState {
    */
   getTotalPrice = () => {
     let price = 0;
-    this.products.forEach((p) => price += (p.price * p.quantity));
+    this.products.forEach((p) => (price += p.price * p.quantity));
     return price;
-  }
+  };
 
   /**
    * Adds a product to the cart
@@ -21,7 +21,7 @@ export default class CartState {
    * @param q Number of products to add
    */
   addProduct = (p: Product, q = 1): void => {
-    let exists = false
+    let exists = false;
     this.products = this.products.map((prd) => {
       if (prd.id === p.id) {
         exists = true;
@@ -32,7 +32,7 @@ export default class CartState {
     if (!exists) {
       this.products.push(new CartProduct(p, q));
     }
-  }
+  };
 
   /**
    * Removes a product from the cart
@@ -46,27 +46,34 @@ export default class CartState {
       return true;
     }
     return false;
-  }
+  };
 
   /**
-  * Sets quantity of a product
-  * @param id ID of the product to set quantity of
-  * @param quantity new quantity of the product
-  * @returns true if edit successful, false otherwise
-  */
+   * Clears the cart
+   */
+  clearCart = () => {
+    this.products = [];
+  };
+
+  /**
+   * Sets quantity of a product
+   * @param id ID of the product to set quantity of
+   * @param quantity new quantity of the product
+   * @returns true if edit successful, false otherwise
+   */
   setProductQuantity = (id: number, quantity: number): boolean => {
     if (this.getProduct(id)) {
       this.products.some((p, i) => {
         if (p.id === id) {
-          this.products[i].quantity = quantity
+          this.products[i].quantity = quantity;
           return true;
-        };
+        }
         return false;
-      })
+      });
       return true;
     }
     return false;
-  }
+  };
 
   /**
    * Gets a product from the cart
@@ -75,11 +82,14 @@ export default class CartState {
    */
   getProduct = (id: number): CartProduct | false => {
     let index = -1;
-    this.products.some((p, i) => { index = i; return p.id === id });
+    this.products.some((p, i) => {
+      index = i;
+      return p.id === id;
+    });
     if (index !== -1) {
       return this.products[index];
     } else {
       return false;
     }
-  }
+  };
 }
