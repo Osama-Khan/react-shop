e0da520ff8d20378da8c7756f05516a15e366b43
Osama Khan <Osama6756@gmail.com>
2021-06-10 17:12:11 +0500

    Improved User page UI, Added viewing other users based on ID

--- src/app/user/user.jsx
+++ src/app/user/user.jsx
@@ -2,126 +2,165 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { PrimaryButton } from "../components/button/Button";
 import { AppContext } from "../context/app.provider";
-import LoadingIcon from "../components/loading/loading";
 import {
-  productsUrl,
   registerUrl,
   userUrl,
   addressesUrl,
   editUserUrl,
+  ordersUrl,
 } from "../routes";
 import Icon from "../components/icon/icon";
 import LoadingSpinner from "../components/loading/loading";
+import Card from "../components/card/card";
 
 export default class User extends React.Component {
   static contextType = AppContext;
-  failed = false;
-  fetching = false;
+  userId;
 
   constructor(props) {
     super(props);
-    this.state = { recentProduct: undefined, address: undefined };
+    this.state = {
+      address: undefined,
+      user: undefined,
+      failed: false,
+      fetching: false,
+    };
   }
 
   render() {
-    if (this.context.state.user.username) {
-      if (
-        this.state.recentProduct === undefined ||
-        this.state.address === undefined
-      ) {
-        if (this.failed) {
-          return <div>Failed to load</div>;
+    if (this.context.state.user.username || this.props.match.params.id) {
+      if (this.state.address === undefined && this.state.user === undefined) {
+        if (this.state.failed) {
+          return this.failedTemplate();
         } else {
-          if (!this.fetching) this.fetchData();
+          if (!this.state.fetching) this.fetchData();
           return <LoadingSpinner />;
         }
       }
-      return this.profileTemplate(this.context.state.user);
+      if (
+        !this.props.match.params.id ||
+        (this.context.state.user.token &&
+          this.state.user?.id === this.context.state.user.id)
+      ) {
+        return this.profileTemplateOwn(this.context.state.user);
+      } else {
+        if (this.state.user) return this.profileTemplateOther(this.state.user);
+        else if (this.state.fetching) return <LoadingSpinner />;
+        else return this.failedTemplate();
+      }
     } else {
       return this.loginTemplate();
     }
   }
 
-  profileTemplate = (user) => (
-    <div className="mt-5 row container d-flex justify-content-center">
-      <div className="col-md-8">
-        <div className="card border border-primary">
-          <div className="row ml-0 mr-0 bg-light">
-            <div className="col-sm-4 bg-primary d-flex flex-column m-0 card rounded-left">
-              <div className="m-2 top-right">
-                <Link to={editUserUrl}>
-                  <Icon
-                    classes="text-subtle-white clickable"
-                    dataIcon="bx-bxs-message-square-edit"
-                  />
-                </Link>
-              </div>
-              <div className="text-center text-white my-auto">
-                <img
-                  src={user.profileImage}
-                  className="rounded-circle shadow img-large"
-                  alt="User-Profile"
+  failedTemplate = () => {
+    return (
+      <div className="mt-5">
+        <div className="alert alert-danger m-auto">
+          <Icon dataIcon="fa:times-circle" />
+          <span className="ml-2">Failed to load</span>
+        </div>
+      </div>
+    );
+  };
+
+  profileTemplateOwn = (user) => (
+    <div className="mt-5">
+      <div className="card border border-primary m-auto col-md-8 p-0">
+        <div className="row ml-0 mr-0 bg-light">
+          <div className="col-sm-4 bg-primary d-flex flex-column m-0 card rounded-left">
+            <div className="m-2 top-right">
+              <Link to={editUserUrl}>
+                <Icon
+                  classes="text-subtle-white clickable"
+                  dataIcon="bx-bxs-message-square-edit"
                 />
-                <h6 className="mt-1 font-weight-bold">
-                  {user.firstName} {user.lastName}
-                </h6>
-                <Link to={`${userUrl}/${user.id}`}>
-                  <p className="badge btn btn-light">@{user.username}</p>
-                </Link>
+              </Link>
+            </div>
+            <div className="text-center text-white my-auto">
+              <img
+                src={user.profileImage}
+                className="rounded-circle shadow img-large"
+                alt="User-Profile"
+              />
+              <h6 className="mt-1 font-weight-bold">
+                {user.firstName} {user.lastName}
+              </h6>
+              <Link to={`${userUrl}/${user.id}`}>
+                <p className="badge btn btn-light">@{user.username}</p>
+              </Link>
+            </div>
+          </div>
+          <div className="col-sm-8">
+            <h6 className="mt-3 font-weight-bold">Information</h6>
+            <div className="row">
+              <div className="col-sm-6">
+                <p className="mb-0">Email</p>
+                <h6 className="text-muted">{user.email}</h6>
+              </div>
+              <div className="col-sm-6">
+                <p className="mb-0">
+                  Default Address{" "}
+                  <Link to={addressesUrl}>
+                    <Icon
+                      dataIcon="bx-bxs-message-square-detail"
+                      classes="text-transparent-dark clickable"
+                    />
+                  </Link>
+                </p>
+                <p>
+                  <b>{this.state.address ? this.state.address.tag : ""}</b>{" "}
+                  <span className="text-sm text-muted">
+                    {" "}
+                    {this.state.address
+                      ? this.state.address.address
+                      : "No default address"}
+                  </span>
+                </p>
               </div>
             </div>
-            <div className="col-sm-8">
-              <h6 className="mt-3 font-weight-bold">Information</h6>
-              <div className="row">
-                <div className="col-sm-6">
-                  <p className="mb-0">Email</p>
-                  <h6 className="text-muted">{user.email}</h6>
-                </div>
-                <div className="col-sm-6">
-                  <p className="mb-0">
-                    Default Address{" "}
-                    <Link to={addressesUrl}>
-                      <Icon
-                        dataIcon="bx-bxs-message-square-detail"
-                        classes="text-transparent-dark clickable"
-                      />
-                    </Link>
-                  </p>
-                  <p>
-                    <b>{this.state.address ? this.state.address.tag : ""}</b>{" "}
-                    <span className="text-sm text-muted">
-                      {" "}
-                      {this.state.address
-                        ? this.state.address.address
-                        : "No default address"}
-                    </span>
-                  </p>
-                </div>
+            <hr />
+            <div className="row mt-2">
+              <div className="col-sm-6 mb-3 mx-auto">
+                <Card
+                  text="My Products"
+                  icon="bx-bxs-box"
+                  color="primary"
+                  iconClasses="icon-sm"
+                  linkTo={`${userUrl}/${user.id}/products`}
+                />
               </div>
-              <hr />
-              <h6 className="mt-2 font-weight-bold">Products</h6>
-              <div className="row">
-                <div className="col-sm-6 mb-3">
-                  <p className="mb-0">Recent</p>
-                  {this.state.recentProduct !== undefined ? (
-                    this.state.recentProduct ? (
-                      <Link
-                        className="text-sm text-muted"
-                        to={`${productsUrl}/${this.state.recentProduct.id}`}>
-                        {this.state.recentProduct.code}
-                      </Link>
-                    ) : (
-                      <span className="text-sm text-muted">
-                        This user has no products
-                      </span>
-                    )
-                  ) : (
-                    <LoadingIcon />
-                  )}
-                </div>
+              <div className="col-sm-6 mb-3 mx-auto">
+                <Card
+                  text="My Orders"
+                  icon="fa-solid:file-invoice"
+                  color="primary"
+                  iconClasses="icon-sm"
+                  linkTo={ordersUrl}
+                />
               </div>
             </div>
           </div>
+        </div>
+      </div>
+    </div>
+  );
+
+  profileTemplateOther = (user) => (
+    <div className="mt-5">
+      <div className="col-sm-4 bg-primary d-flex flex-column m-auto card">
+        <div className="text-center text-white my-auto">
+          <img
+            src={user.profileImage}
+            className="rounded-circle shadow img-large"
+            alt="User-Profile"
+          />
+          <h6 className="mt-1 font-weight-bold">
+            {user.firstName} {user.lastName}
+          </h6>
+          <Link to={`${userUrl}/${user.id}`}>
+            <p className="badge btn btn-light">@{user.username}</p>
+          </Link>
         </div>
       </div>
     </div>
@@ -161,47 +200,59 @@ export default class User extends React.Component {
   );
 
   fetchData = () => {
-    this.fetching = true;
-    const user = this.context.state.user;
-    this.context.services.settingService
-      .getDefaultAddress(user.id)
-      .then((address) => {
-        if (address) {
-          this.setState({ ...this.state, address });
-          this.context.services.addressService
-            .getAddress(address.id)
-            .then((address) => {
-              this.setState({ ...this.state, address });
-            })
-            .catch((err) => {
-              if (err) {
-                this.failed = true;
-              }
-            });
-          return;
-        }
-        this.setState({
-          ...this.state,
-          address: false,
+    this.setState({ ...this.state, fetching: true });
+    const id = this.props.match.params.id;
+    if (id) {
+      console.log("ID" + id);
+      this.context.services.userService
+        .getUser(parseInt(id))
+        .then((user) => {
+          this.setState({ ...this.state, user });
+        })
+        .catch((err) => {
+          if (err) {
+            if (err.response.status === 404) {
+              this.context.services.uiService.iconModal(
+                "Nobody Here",
+                "The user you are looking for could not be found",
+                "error"
+              );
+            }
+            this.setState({ ...this.state, failed: true });
+          }
+        })
+        .finally(() => this.setState({ ...this.state, fetching: false }));
+    } else {
+      const user = this.context.state.user;
+      this.context.services.settingService
+        .getDefaultAddress(user.id)
+        .then((address) => {
+          if (address) {
+            this.setState({ ...this.state, address });
+            this.context.services.addressService
+              .getAddress(address.id)
+              .then((address) => {
+                this.setState({ ...this.state, address });
+              })
+              .catch((err) => {
+                if (err) {
+                  this.setState({ ...this.state, failed: true });
+                }
+              })
+              .finally(() => (this.fetching = false));
+            return;
+          }
+          this.setState({
+            ...this.state,
+            address: false,
+          });
+        })
+        .catch((err) => {
+          if (err) {
+            this.setState({ ...this.state, failed: true });
+          }
         });
-      })
-      .catch((err) => {
-        if (err) {
-          this.failed = true;
-        }
-      });
-    this.context.services.userService
-      .fetchMostRecentProduct(user.id)
-      .then((recentProduct) => {
-        this.setState({ ...this.state, recentProduct });
-      })
-      .catch((err) => {
-        if (err.response.status === 404) {
-          this.setState({ ...this.state, recentProduct: false });
-          return;
-        }
-        this.failed = true;
-      });
+    }
   };
 
   login = () => {
