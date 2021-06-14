accdfbea4f511e55a08aa9c65efd05e6c7a6bce9
Osama Khan <Osama6756@gmail.com>
2021-06-14 17:10:08 +0500

    Added more customization to Loading Spinner

--- src/app/components/loading/loading.jsx
+++ src/app/components/loading/loading.jsx
@@ -1,9 +1,11 @@
 import Icon from "../icon/icon";
 
-export default function LoadingSpinner() {
+export default function LoadingSpinner({ size = "lg", inline = false }) {
   return (
-    <div key="loadingIcon" className="col-12 d-flex my-5 py-5">
-      <Icon classes="mx-auto icon-lg spin" dataIcon="fa:spinner" />
+    <div
+      key="loadingIcon"
+      className={inline ? "my-auto mx-1" : "col-12 d-flex my-5 py-5"}>
+      <Icon classes={`mx-auto icon-${size} spin`} dataIcon="fa:spinner" />
     </div>
   );
 }
