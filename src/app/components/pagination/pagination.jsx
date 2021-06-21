d68c483216ce53cf33b5ea2dd721dd85c3995776
Osama Khan <Osama6756@gmail.com>
2021-06-22 01:28:32 +0500

    Added Pagination component

---
+++ src/app/components/pagination/pagination.jsx
@@ -0,0 +1,68 @@
+export default function Pagination({ currentPage, totalPages, gotoPage }) {
+  const trimForward = totalPages > 3 && totalPages - currentPage + 1 > 3;
+  const trimBackward = totalPages > 3 && totalPages - currentPage + 1 < 3;
+  const isFirst = currentPage === 1;
+  const isLast = currentPage === totalPages;
+  const maxPages = trimForward ? currentPage + 3 : totalPages + 1;
+  let numEls = [];
+  if (trimBackward && !isFirst) {
+    numEls.push(
+      <li className="page-item">
+        <span className="page-link">...</span>
+      </li>
+    );
+  }
+  for (
+    let i = !isFirst
+      ? isLast && totalPages > 3
+        ? currentPage - 2
+        : currentPage - 1
+      : currentPage;
+    i < maxPages;
+    i++
+  ) {
+    numEls.push(
+      <li className={`page-item${currentPage === i ? " active" : ""}`}>
+        <span
+          className="page-link text-clickable"
+          onClick={() => {
+            if (currentPage !== i) gotoPage(i);
+          }}>
+          {i}
+        </span>
+      </li>
+    );
+  }
+  if (trimForward && !isLast) {
+    numEls.push(
+      <li className="page-item">
+        <span className="page-link">...</span>
+      </li>
+    );
+  }
+  return (
+    <div className="shadow">
+      <ul class="pagination">
+        <li class={`page-item${isFirst ? " disabled" : ""}`}>
+          <span
+            className="page-link text-clickable"
+            onClick={() => {
+              gotoPage(1);
+            }}>
+            First
+          </span>
+        </li>
+        {numEls}
+        <li class={`page-item${isLast ? " disabled" : ""}`}>
+          <span
+            className="page-link text-clickable"
+            onClick={() => {
+              gotoPage(totalPages);
+            }}>
+            Last
+          </span>
+        </li>
+      </ul>
+    </div>
+  );
+}
