a034fb80dca363617d802af47d4a6c68d811901d
Osama Khan <Osama6756@gmail.com>
2021-06-22 09:12:46 +0500

    Made OrderList more generic

--- src/app/order/order-list.jsx
+++ src/app/order/order-list.jsx
@@ -4,70 +4,63 @@ import OrderStateBadge from "./order-state-badge";
 import { Link } from "react-router-dom";
 
 export default function OrderList({ state }) {
-  const orderEls =
-    state.orders.length > 0 ? (
-      state.orders.map((o) => {
-        let total = 0;
-        return (
-          <div className="col-md-8 card mb-4 mx-auto p-0">
-            <div className="card-header bg-dark text-light row">
-              <h1 className="m-0 ml-2">Order #{o.id} </h1>
-              <OrderStateBadge orderState={o.orderState} />
-            </div>
-            <div className="mb-2 p-2">
-              {state.detailLoaded ? (
-                o.orderProducts.map((op) => {
-                  total += op.price * op.quantity;
-                  return (
-                    <>
-                      <br />
-                      <span className="ml-2">
-                        {" "}
-                        {op.quantity} &times;{" "}
-                        <Link
-                          to={`${productsUrl}/${op.product.id}`}
-                          title={op.product.title}>
-                          {op.product.title.substring(0, 16) + "..."}
-                        </Link>
-                      </span>{" "}
-                      = <b>{op.price} Rs.</b>
-                    </>
-                  );
-                })
-              ) : (
-                <LoadingSpinner />
-              )}
-            </div>
-            <div className="row m-0 px-2">
-              <span className="my-auto text-muted">
-                Placed on: {new Date(o.createdAt).toLocaleString()}
-                <br />
-                Last Update: {new Date(o.updatedAt).toLocaleString()}
-              </span>
-              <span className="bg-green-subtle badge-pill m-3 ml-auto p-2 text-green">
-                <b>{total}</b> Rs.
-              </span>
-            </div>
+  return state.orders.length > 0 ? (
+    state.orders.map((o) => {
+      let total = 0;
+      return (
+        <div className="col-md-8 card mb-4 mx-auto p-0">
+          <div className="card-header bg-dark text-light row">
+            <h1 className="m-0 ml-2">Order #{o.id} </h1>
+            <OrderStateBadge orderState={o.orderState} />
           </div>
-        );
-      })
-    ) : (
-      <>
-        <div className="text-center text-muted font-weight-bold mt-5">
-          You have placed no orders... yet!
+          <div className="mb-2 p-2">
+            {state.detailLoaded ? (
+              o.orderProducts.map((op) => {
+                total += op.price * op.quantity;
+                return (
+                  <>
+                    <br />
+                    <span className="ml-2">
+                      {" "}
+                      {op.quantity} &times;{" "}
+                      <Link
+                        to={`${productsUrl}/${op.product.id}`}
+                        title={op.product.title}>
+                        {op.product.title.substring(0, 16) + "..."}
+                      </Link>
+                    </span>{" "}
+                    = <b>{op.price} Rs.</b>
+                  </>
+                );
+              })
+            ) : (
+              <LoadingSpinner />
+            )}
+          </div>
+          <div className="row m-0 px-2">
+            <span className="my-auto text-muted">
+              Placed on: {new Date(o.createdAt).toLocaleString()}
+              <br />
+              Last Update: {new Date(o.updatedAt).toLocaleString()}
+            </span>
+            <span className="bg-green-subtle badge-pill m-3 ml-auto p-2 text-green">
+              <b>{total}</b> Rs.
+            </span>
+          </div>
         </div>
-        <div className="d-flex">
-          {" "}
-          <Link className="btn btn-dark mx-auto mt-3" to={productsUrl}>
-            Go Get Something
-          </Link>
-        </div>
-      </>
-    );
-  return (
-    <div className="mt-5">
-      <h2>Your Orders</h2>
-      <div className="p-3">{orderEls}</div>
-    </div>
+      );
+    })
+  ) : (
+    <>
+      <div className="text-center text-muted font-weight-bold mt-5">
+        You have placed no orders... yet!
+      </div>
+      <div className="d-flex">
+        {" "}
+        <Link className="btn btn-dark mx-auto mt-3" to={productsUrl}>
+          Go Get Something
+        </Link>
+      </div>
+    </>
   );
 }
