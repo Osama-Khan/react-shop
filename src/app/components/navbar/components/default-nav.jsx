33266be9387c72aef9c85807676e62231f5210e3
Osama Khan <Osama6756@gmail.com>
2021-06-23 16:20:20 +0500

    Improved Navbar components
    Broke down the Navbar component in to smaller, separate components for Mobile and Default navs

---
+++ src/app/components/navbar/components/default-nav.jsx
@@ -0,0 +1,144 @@
+import {
+  categoriesUrl,
+  homeUrl,
+  productsUrl,
+  registerUrl,
+  userUrl,
+} from "../../../routes";
+import { PrimaryButton } from "../../button/Button";
+import { Link } from "react-router-dom";
+import Icon from "../../icon/icon";
+import Popup from "../../popup/popup";
+import NavbarCartCard from "./navbar-cart-card";
+
+const NavAccountContent = ({ context }) => (
+  <div>
+    <p className="text-center font-weight-bold">ACCOUNT</p>
+    <hr />
+    <div className="form-group">
+      <label>Username</label>
+      <input
+        className="form-control"
+        type="text"
+        id="loginPopupUsernameField"
+      />
+    </div>
+    <div className="form-group">
+      <label>Password</label>
+      <input
+        className="form-control"
+        type="password"
+        id="loginPopupPasswordField"
+      />
+      <label className="ml-auto text-sm text-clickable">Forgot password?</label>
+    </div>
+    <PrimaryButton
+      text="Login"
+      click={() => login(context)}
+      classes="btn-block"></PrimaryButton>
+    <Link to={registerUrl}>
+      <PrimaryButton
+        text="Create an account"
+        outline={true}
+        classes="btn-block"></PrimaryButton>
+    </Link>
+  </div>
+);
+
+export default function DefaultNav({ context, loc }) {
+  return (
+    <ul className="d-none d-md-flex nav container">
+      <li className="nav-logo m-1">
+        <Link to="/" className="anchor-color-remover">
+          <Icon dataIcon="cib:stripe-s" />
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          className={`nav-link ${loc === homeUrl ? "active" : ""}`}
+          to={homeUrl}>
+          <Icon dataIcon="ant-design:home-filled" /> Home
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          className={`nav-link ${loc === productsUrl ? "active" : ""}`}
+          to={productsUrl}>
+          <Icon dataIcon="ant-design:shopping-filled" /> Products
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          className={`nav-link ${loc === categoriesUrl ? "active" : ""}`}
+          to={categoriesUrl}>
+          <Icon dataIcon="bx:bxs-category-alt" /> Categories
+        </Link>
+      </li>
+      <li className="nav-item ml-auto">
+        <Popup
+          trigger={
+            <div className={`nav-link text-clickable`}>
+              <Icon dataIcon="mi-shopping-cart" />
+            </div>
+          }
+          content={<NavbarCartCard />}
+          parent="nav"
+        />
+      </li>
+      <li className="nav-item">
+        {context.state.user.token ? (
+          <Link to={userUrl} className="bg-primary-subtle rounded">
+            <div className={`nav-link text-clickable`}>
+              <img
+                className="iconify img-tiny rounded-circle shadow-sm"
+                src={context.state.user.profileImage}
+                alt={context.state.user.username}
+              />
+              <span className="ml-1">{context.state.user.username}</span>
+            </div>
+          </Link>
+        ) : (
+          <Popup
+            trigger={
+              <div className={`nav-link text-clickable`}>
+                <Icon dataIcon="mi-user" />
+                {context.state.user.token ? (
+                  <span className="ml-1">{context.state.user.username}</span>
+                ) : (
+                  ""
+                )}
+              </div>
+            }
+            content={<NavAccountContent context={context} />}
+            position="bottom right"
+            parent="nav"
+          />
+        )}
+      </li>
+    </ul>
+  );
+}
+
+const login = (context) => {
+  const userSvc = context.services.userService;
+  const uiSvc = context.services.uiService;
+  const username = document.getElementById("loginPopupUsernameField").value;
+  const password = document.getElementById("loginPopupPasswordField").value;
+  const messages = {
+    loading: "Hold on, logging you in!",
+    success: "You are logged in!",
+    error: "We couldn't log you in.",
+  };
+  const loginPromise = userSvc.login(username, password);
+  loginPromise
+    .then((res) => {
+      const user = res.data;
+      context.setState({ ...context.state, user });
+    })
+    .catch((err) => {
+      if (err) {
+        // Couldn't log in
+      }
+    });
+  uiSvc.promiseToast(loginPromise, messages);
+};
---
+++ src/app/components/navbar/components/mobile-nav.jsx
@@ -0,0 +1,85 @@
+import {
+  categoriesUrl,
+  checkoutUrl,
+  homeUrl,
+  productsUrl,
+  userUrl,
+} from "../../../routes";
+import Icon from "../../icon/icon";
+import { Link } from "react-router-dom";
+
+export default function MobileNav({ loc }) {
+  return (
+    <>
+      <ul className="d-flex d-sm-flex d-md-none nav container p-0">
+        <li className="nav-logo m-1">
+          <Link to="/" className="anchor-color-remover">
+            <Icon dataIcon="cib:stripe-s" />
+          </Link>
+        </li>
+        <li className="ml-auto my-auto">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <Icon dataIcon="eva-menu-2-outline" classes="icon-sm" />
+          </button>
+        </li>
+      </ul>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item p-0 my-1">
+            <Link
+              className={`px-2 py-3 w-100 nav-link ${
+                loc === homeUrl ? "active" : ""
+              }`}
+              to={homeUrl}>
+              <Icon dataIcon="ant-design:home-filled" /> Home
+            </Link>
+          </li>
+          <li className="nav-item p-0 my-1">
+            <Link
+              className={`px-2 py-3 w-100 nav-link ${
+                loc === productsUrl ? "active" : ""
+              }`}
+              to={productsUrl}>
+              <Icon dataIcon="ant-design:shopping-filled" /> Products
+            </Link>
+          </li>
+          <li className="nav-item p-0 my-1">
+            <Link
+              className={`px-2 py-3 w-100 nav-link ${
+                loc === categoriesUrl ? "active" : ""
+              }`}
+              to={categoriesUrl}>
+              <Icon dataIcon="bx:bxs-category-alt" /> Categories
+            </Link>
+          </li>
+          <li className="border border-bottom m-1"></li>
+          <li className="nav-item p-0 my-1">
+            <Link
+              className={`px-2 py-3 w-100 nav-link ${
+                loc === userUrl ? "active" : ""
+              }`}
+              to={userUrl}>
+              <Icon dataIcon="mi-user" /> Account
+            </Link>
+          </li>
+          <li className="nav-item p-0 my-1">
+            <Link
+              className={`px-2 py-3 w-100 nav-link ${
+                loc === checkoutUrl ? "active" : ""
+              }`}
+              to={checkoutUrl}>
+              <Icon dataIcon="mi-shopping-cart" /> Cart
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
