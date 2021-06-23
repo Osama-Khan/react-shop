760c8b61a741ae9253a2cfd38c4f2bd812b4007e
Osama Khan <Osama6756@gmail.com>
2021-06-23 14:11:15 +0500

    Improved Pagination component consistency

--- src/app/components/pagination/pagination.jsx
+++ src/app/components/pagination/pagination.jsx
@@ -1,3 +1,17 @@
+const PaginationElement = ({ text, className, liClassName, onClick }) => (
+  <li className={`page-item ${className}`}>
+    <span
+      className={`page-link text-clickable ${liClassName}`}
+      onClick={onClick}>
+      {text}
+    </span>
+  </li>
+);
+
+const MoreIndicator = () => (
+  <PaginationElement text="..." liClassName="text-dark" />
+);
+
 export default function Pagination({ currentPage, totalPages, gotoPage }) {
   const trimForward = totalPages > 3 && totalPages - currentPage + 1 > 3;
   const trimBackward = totalPages > 3 && totalPages - currentPage + 1 < 3;
@@ -5,63 +19,51 @@ export default function Pagination({ currentPage, totalPages, gotoPage }) {
   const isLast = currentPage === totalPages;
   const maxPages = trimForward ? currentPage + 3 : totalPages + 1;
   let numEls = [];
+
   if (trimBackward && !isFirst) {
+    numEls.push(<MoreIndicator />);
+  }
+
+  const iStart = !isFirst
+    ? isLast && totalPages > 3
+      ? currentPage - 2
+      : currentPage - 1
+    : currentPage;
+
+  for (let i = iStart; i < maxPages; i++) {
     numEls.push(
-      <li className="page-item">
-        <span className="page-link">...</span>
-      </li>
+      <PaginationElement
+        className={currentPage === i ? " active" : ""}
+        onClick={() => {
+          if (currentPage !== i) gotoPage(i);
+        }}
+        text={i}
+      />
     );
   }
-  for (
-    let i = !isFirst
-      ? isLast && totalPages > 3
-        ? currentPage - 2
-        : currentPage - 1
-      : currentPage;
-    i < maxPages;
-    i++
-  ) {
-    numEls.push(
-      <li className={`page-item${currentPage === i ? " active" : ""}`}>
-        <span
-          className="page-link text-clickable"
-          onClick={() => {
-            if (currentPage !== i) gotoPage(i);
-          }}>
-          {i}
-        </span>
-      </li>
-    );
-  }
+
   if (trimForward && !isLast) {
-    numEls.push(
-      <li className="page-item">
-        <span className="page-link text-dark">...</span>
-      </li>
-    );
+    numEls.push(<MoreIndicator />);
   }
+
   return (
     <div className="shadow">
       <ul class="pagination">
-        <li class={`page-item${isFirst ? " disabled" : ""}`}>
-          <span
-            className="page-link text-clickable"
-            onClick={() => {
-              gotoPage(1);
-            }}>
-            First
-          </span>
-        </li>
+        <PaginationElement
+          className={isFirst ? " disabled" : ""}
+          text="First"
+          onClick={() => {
+            gotoPage(1);
+          }}
+        />
         {numEls}
-        <li class={`page-item${isLast ? " disabled" : ""}`}>
-          <span
-            className="page-link text-clickable"
-            onClick={() => {
-              gotoPage(totalPages);
-            }}>
-            Last
-          </span>
-        </li>
+        <PaginationElement
+          className={isLast ? " disabled" : ""}
+          text="Last"
+          onClick={() => {
+            gotoPage(totalPages);
+          }}
+        />
       </ul>
     </div>
   );
