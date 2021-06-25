3cef62b8215adc804f4ec2e976d4341277ec447b
Osama Khan <Osama6756@gmail.com>
2021-06-25 13:16:59 +0500

    Added cart related functionality to StorageService

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,5 +1,8 @@
+import CartState from "../state/cart-state";
+
 export default class StorageService {
   userTokenKey = "user-token";
+  cartKey = "cart";
 
   /**
    * Saves the given token in localStorage
@@ -17,4 +20,32 @@ export default class StorageService {
    * Removes the user-token from storage
    */
   clearUserToken = () => localStorage.removeItem(this.userTokenKey);
+
+  /**
+   * Saves cart product IDs and Quantities in localStorage
+   * @param cart The state of the cart
+   */
+  updateCart = (cart: CartState) => {
+    if (cart.products?.length > 0)
+      localStorage.setItem(
+        this.cartKey,
+        cart.products.map((p) => p.id + ":" + p.quantity).join(";")
+      );
+    else this.clearCart();
+  };
+
+  /** Removes cart data from localStorage */
+  clearCart = () => localStorage.removeItem(this.cartKey);
+
+  /**
+   * Loads cart product IDs and Quantities saved on last session
+   * @returns list of objects containing product id and quantity
+   */
+  loadCart = () => {
+    const items = localStorage.getItem(this.cartKey)?.split(";");
+    return items?.map((i) => {
+      const data = i.split(":").map((n) => parseInt(n));
+      return { id: data[0], quantity: data[1] };
+    });
+  };
 }
