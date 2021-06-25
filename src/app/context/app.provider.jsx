e26d9118f3337c23e30d6def0663880a020bcc55
Osama Khan <Osama6756@gmail.com>
2021-06-25 13:18:15 +0500

    Added effect to update local cart data on changes

--- src/app/context/app.provider.jsx
+++ src/app/context/app.provider.jsx
@@ -1,4 +1,4 @@
-import { useState, createContext } from "react";
+import { useState, useEffect, createContext } from "react";
 import AddressService from "../services/address.service";
 import CategoryService from "../services/category.service";
 import FavoriteService from "../services/favorite.service";
@@ -15,19 +15,28 @@ export const AppContext = createContext();
 
 export const AppProvider = (props) => {
   const [state, setState] = useState(new State());
+  const services = {
+    addressService: new AddressService(),
+    categoryService: new CategoryService(),
+    favoriteService: new FavoriteService(),
+    locationService: new LocationService(),
+    orderService: new OrderService(),
+    productService: new ProductService(),
+    settingService: new SettingService(),
+    storageService: new StorageService(),
+    uiService: new UiService(),
+    userService: new UserService(),
+  };
+
+  useEffect(() => {
+    if (state.cart?.products.length > 0) {
+      services.storageService.updateCart(state.cart);
+    } else {
+      services.storageService.clearCart();
+    }
+  }, [state, services.storageService]);
   const context = {
-    services: {
-      addressService: new AddressService(),
-      categoryService: new CategoryService(),
-      favoriteService: new FavoriteService(),
-      locationService: new LocationService(),
-      orderService: new OrderService(),
-      productService: new ProductService(),
-      settingService: new SettingService(),
-      storageService: new StorageService(),
-      uiService: new UiService(),
-      userService: new UserService(),
-    },
+    services,
     state,
     setState,
   };
