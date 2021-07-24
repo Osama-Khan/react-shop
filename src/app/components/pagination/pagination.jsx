e8c3a30e530b77ec7e6623292fb23a5b2c403e29
Osama Khan <Osama6756@gmail.com>
2021-07-24 21:18:28 +0500

    Improved Pagination, fixed bug: MoreIndicator not shown in start

--- src/app/components/pagination/pagination.jsx
+++ src/app/components/pagination/pagination.jsx
@@ -1,3 +1,4 @@
+/** Clickable pagination element */
 const PaginationElement = ({ text, className, liClassName, onClick }) => (
   <li className={`page-item ${className}`}>
     <span
@@ -8,31 +9,42 @@ const PaginationElement = ({ text, className, liClassName, onClick }) => (
   </li>
 );
 
+/** A pagination element indicating that there are more pages in between */
 const MoreIndicator = () => (
   <PaginationElement text="..." liClassName="text-dark" />
 );
 
+/** Converts given metadata into a Pagination component */
 export default function Pagination({ currentPage, totalPages, gotoPage }) {
+  // If total pages are 1, there is no need for pagination
   if (totalPages <= 1) {
     return <></>;
   }
-  const trimForward = totalPages > 3 && totalPages - currentPage + 1 > 3;
-  const trimBackward = totalPages > 3 && totalPages - currentPage + 1 < 3;
+
+  // Conditions to decide what elements to add in pagination
+  const showMore = totalPages > 3;
+  const trimStart = showMore && currentPage > 2;
+  const trimEnd = showMore && totalPages - currentPage + 1 > 3;
   const isFirst = currentPage === 1;
   const isLast = currentPage === totalPages;
-  const maxPages = trimForward ? currentPage + 3 : totalPages + 1;
+  const maxPages = trimEnd ? currentPage + 3 : totalPages + 1;
   let numEls = [];
 
-  if (trimBackward && !isFirst) {
+  // Show more indicator if start should be trimmed and selected page is not first
+  if (trimStart && !isFirst) {
     numEls.push(<MoreIndicator key="pagination-el-more-1" />);
   }
 
-  const iStart = !isFirst
-    ? isLast && totalPages > 3
+  // Constant that decides where loop iteration will start
+  const iStart = !showMore
+    ? 1
+    : !isFirst
+    ? isLast
       ? currentPage - 2
       : currentPage - 1
     : currentPage;
 
+  // Places the numbered clickable pagination elements
   for (let i = iStart; i < maxPages; i++) {
     numEls.push(
       <PaginationElement
@@ -46,7 +58,8 @@ export default function Pagination({ currentPage, totalPages, gotoPage }) {
     );
   }
 
-  if (trimForward && !isLast) {
+  // Show more indicator if end should be trimmed and selected page is not last
+  if (trimEnd && !isLast) {
     numEls.push(<MoreIndicator key="pagination-el-more-2" />);
   }
 
