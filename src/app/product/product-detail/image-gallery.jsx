d68087f6b69522677358be4a6c4dc592f654ee0b
Osama Khan <Osama6756@gmail.com>
2021-07-27 03:02:06 +0500

    Improved image gallery styles

--- src/app/product/product-detail/image-gallery.jsx
+++ src/app/product/product-detail/image-gallery.jsx
@@ -8,24 +8,28 @@ export default class ImageGallery extends Component {
 
   render() {
     if (!this.props.images?.length > 0)
-      return <div className="m-3 p-3 text-center card">No images</div>;
+      return (
+        <div className="m-3 py-5 text-center bg-light text-muted">
+          No images
+        </div>
+      );
     const selected = this.state.selectedImage;
     const images = this.props.images.map((i) => i.image);
     const imgSelectors = images.map((img, i) => (
       <div
         key={'img-' + i}
-        className={`card col-3 mr-1 my-1${
+        className={`card p-0 col-3 mr-1 my-1${
           selected !== i ? ' opacity-3' : ' border-primary'
         }`}
         onMouseOver={() => {
           this.setState({ selectedImage: i });
         }}>
-        <img src={img} className="img-small" alt={'image-' + i} />
+        <img src={img} className="img-small-w m-auto" alt={'image-' + i} />
       </div>
     ));
     return (
-      <div id="gallery" className="m-3">
-        <div className="text-center card border-primary">
+      <div className="gallery m-3">
+        <div className="main-img text-center card border-primary">
           <img
             src={images[this.state.selectedImage]}
             className="m-auto p-3"
